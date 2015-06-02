372d0ed5eab3eca1778363dc44f33a9676b8cab8
Sergi Pasoev <s.pasoev@gmail.com>
2015-06-02 19:15:52 +0400

    rudimental support of dynamically loading game objecs specified by xml level files

--- Makefile
+++ Makefile
@@ -3,14 +3,15 @@ CC=gcc
 W=-Wall -Wextra -Wstrict-prototypes -Wshadow -Wwrite-strings \
 -Wold-style-definition -Wredundant-decls -Wnested-externs \
 -Wmissing-include-dirs -Wstrict-aliasing
-CFLAGS=-lSDL2 -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lm -g $(W)
-objects = list.o fsm.o geometry.o sprite.o ledge.o player.o text.o texture.o scaffold.o menustate.o playstate.o pausestate.o gameoverstate.o
+CFLAGS=-I/usr/include/libxml2 -lSDL2 -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lm -g $(W) -lxml2 
+objects = list.o parselvl.o fsm.o geometry.o sprite.o ledge.o player.o text.o texture.o scaffold.o menustate.o playstate.o pausestate.o gameoverstate.o
 
 scaffold : $(objects)
 	$(CC) $(CFLAGS) -o scaffold $(objects)
 
 list.o : list.h
 
+parselvl.o: parselvl.h
 gameoverstate.o : gamestate.h texture.h geometry.h
 geometry.o : geometry.h
 menustate.o : gamestate.h texture.h geometry.h
--- ledge.c
+++ ledge.c
@@ -1,4 +1,5 @@
 #include "ledge.h"
+#include "parselvl.h"
 
 struct Ledge{
   int x;
@@ -10,6 +11,16 @@ struct Ledge{
 
 SDL_Texture *brickTexture;
 
+void addLedge(List *ledges, int x, int y, int w, int h, const char *texturefilename, SDL_Renderer *renderer){
+  struct Ledge *ledge = malloc(sizeof (struct Ledge));
+  ledge->x = x;
+  ledge->y = y;
+  ledge->w = w;
+  ledge->h = h;
+  ledge->texture = brickTexture;
+  list_ins_next(ledges, list_tail(ledges), ledge);
+}
+
 void destroyLedge(void *data){
   struct Ledge *ledge = (struct Ledge*)data;
   SDL_DestroyTexture(ledge->texture);
@@ -20,25 +31,19 @@ void initLedges(List *ledges, SDL_Renderer *renderer){
   /* LoadImage(textureFilename, &((*ledge)->texture), renderer); */
   
   
-  int i;
-  for(i = 0; i < 10; i++){
-    makeLedge(ledges, i*128, 400, 128, 95, "graphics/bricks.png", renderer);
-  }
+  /*  int i;
+      for(i = 0; i < 10; i++){
+      addLedge(ledges, i*128, 400, 128, 95, "graphics/bricks.png", renderer);
+      }
+  */
+  int numLedges = 0;
+  numLedges = parseLedges(ledges, "objects.xml", addLedge);
+  printf("Loaded game objects: %d\n", numLedges);
 }
 
-void makeLedge(List *ledges, int x, int y, int w, int h, const char *texturefilename, SDL_Renderer *renderer){
-  struct Ledge *ledge = malloc(sizeof (struct Ledge));
-  ledge->x = x;
-  ledge->y = y;
-  ledge->w = w;
-  ledge->h = h;
-  ledge->texture = brickTexture;
-  list_ins_next(ledges, list_tail(ledges), ledge);
-}
-  
 void renderLedges(List *ledges, int n, SDL_Renderer *renderer){
   ListElmt *elmt;
-  for(elmt = list_head(ledges); list_next(elmt) != NULL; elmt = list_next(elmt)){
+  for(elmt = list_head(ledges); elmt != NULL; elmt = list_next(elmt)){
     struct Ledge *ledge = (struct Ledge *)list_data(elmt);
     DrawImage(ledge->texture, ledge->x, ledge->y, ledge->w, ledge->h, 0, SDL_FLIP_NONE, renderer);
   } 
