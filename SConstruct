a7465d72703e65ca1ecddcd88c6031ee1ff278e2
Sergi Pasoev <s.pasoev@gmail.com>
2015-11-24 17:22:29 +0400

    a working but bogus implementation of the xml parser with the yxml library

--- SConstruct
+++ SConstruct
@@ -1,7 +1,13 @@
+cc = "gcc"
+ccflags = '-g'
+env = Environment(CC = cc, CCFLAGS = ccflags)
+
+
+
 libs = Split('SDL2 SDL2_image SDL2_gfx SDL2_ttf m xml2')
 libpath = ['/usr/lib', '/usr/local/lib']
 cpppath = ['/usr/include/libxml2']
 
 src_files = Split('list.c yxml.c parselvl.c fsm.c geometry.c sprite.c ledge.c enemy.c player.c text.c texture.c scaffold.c menustate.c playstate.c pausestate.c gameoverstate.c')
-Program(source = src_files, target = 'scaffold', LIBS=libs,
+env.Program(source = src_files, target = 'scaffold', LIBS=libs,
 LIBPATH=libpath, CPPPATH=cpppath)
--- parselvl.c
+++ parselvl.c
@@ -5,21 +5,14 @@
 static char stack[8*1024];
 static int verbose = 0;
 
-char pos_xChr[20] = "", pos_yChr[20] = "", wChr[20] = "", hChr[20] = "";
+char pos_xChr[20] = "", pos_yChr[20] = "", wChr[20] = "", hChr[20] = "", textureFilenameChr[128] = "";
 int pos_x = 0, pos_y = 0, w = 0, h = 0;
+char textureFilename[128] = "";
 
-static void y_printres(yxml_t *x, yxml_ret_t r) {
+static void y_printres(yxml_t *x, yxml_ret_t r, List *ledges) {
 	static int indata;
 	int nextdata = 0;
 
-	/* declare the string for each attribute here. Concatenate on attrval. */
-
-
-	/*
-	  addLedge(ledges, x, y, w, h, textureFilename, NULL);
-	*/
-
-	
 	switch(r) {
 	case YXML_OK:
 		if(verbose) {
@@ -29,34 +22,12 @@ static void y_printres(yxml_t *x, yxml_ret_t r) {
 			nextdata = indata;
 		break;
 	case YXML_ELEMSTART:
-		/* y_printtoken(x, "elemstart "); */
-		/* y_printstring(x->elem); */
-		
-		if(yxml_symlen(x, x->elem) != strlen(x->elem)){
-			/* y_printtoken(x, "assertfail: elem lengths don't match"); */
-		}
-		if(r & YXML_CONTENT){
-			/* y_printtoken(x, "content");*/
-		}
-		
 		break;
 	case YXML_ELEMEND:
+		addLedge(ledges, pos_x, pos_y, w, h, "graphics/bricks.png", NULL);
 		/* printf("\n-----\nx = %d, y = %d, w = %d, h = %d\n-----\n", pos_x, pos_y, w, h); */
-		/* y_printtoken(x, "elemend"); */
-		printf("\n-----\nx = %d, y = %d, w = %d, h = %d\n-----\n", pos_x, pos_y, w, h);
-		break;
-	case YXML_ATTRSTART:
-		/* y_printtoken(x, "attrstart "); */
-		/* y_printstring(x->attr); */
-		/* 
-		if(yxml_symlen(x, x->attr) != strlen(x->attr))
-			y_printtoken(x, "assertfail: attr lengths don't match");
-		*/
 		break;
 	case YXML_ATTREND:
-		/*
-		y_printtoken(x, "attrend");
-		*/
 		if(!strcmp("x", x->attr)){
 			pos_x = atoi(pos_xChr);
 			memset(pos_xChr, 0, sizeof(pos_xChr));
@@ -69,16 +40,14 @@ static void y_printres(yxml_t *x, yxml_ret_t r) {
 		}else if(!strcmp("height", x->attr)){
 			h = atoi(hChr);
 			memset(hChr, 0, sizeof(hChr));
+		}else if(!strcmp("textureFile", x->attr)){
+			strcpy(textureFilename, textureFilenameChr);
+			memset(textureFilenameChr, 0, sizeof(textureFilenameChr));
 		}
 		break;
 	case YXML_PICONTENT:
 	case YXML_CONTENT:
 	case YXML_ATTRVAL:
-		/*
-		if(!indata){
-			y_printtoken(x, r == YXML_CONTENT ? "content " : r == YXML_PICONTENT ? "picontent " : "attrval ");
-		}
-		*/
 		if(!strcmp("x", x->attr)){
 			strcat(pos_xChr, x->data);
 		}else if(!strcmp("y", x->attr)){
@@ -87,47 +56,20 @@ static void y_printres(yxml_t *x, yxml_ret_t r) {
 			strcat(wChr, x->data);
 		}else if(!strcmp("height", x->attr)){
 			strcat(hChr, x->data);
+		}else if(!strcmp("textureFile", x->attr)){
+			strcat(textureFilenameChr, x->data);
 		}
-		/* y_printstring(x->data); */
 		nextdata = 1;
 		break;
 	case YXML_PISTART:
-		/* y_printtoken(x, "pistart "); */
-		/* y_printstring(x->pi); */
-		if(yxml_symlen(x, x->pi) != strlen(x->pi)){
-			/* y_printtoken(x, "assertfail: pi lengths don't match"); */
-		}
 		break;
 	case YXML_PIEND:
-		/*
-		y_printtoken(x, "piend");
-		*/
 		break;
 	default:
-		/*
-		y_printtoken(x, "error\n");
-		*/
-		exit(0);
+		/* exit(0); */
+		printf("Something unexpected happened.");
 	}
 	indata = nextdata;
-}
-
-struct Ledge{
-	int x;
-	int y;
-	int w;
-	int h;
-	SDL_Texture *texture;
-};
-
-void addLedge(List *ledges, int x, int y, int w, int h, const char *texturefilename, SDL_Renderer *renderer){
-	struct Ledge *ledge = malloc(sizeof (struct Ledge));
-	ledge->x = x;
-	ledge->y = y;
-	ledge->w = w;
-	ledge->h = h;
-	ledge->texture = NULL;
-	list_ins_next(ledges, list_tail(ledges), ledge);
 }
 
 int parseLedges(List *ledges, char *filename,
@@ -139,8 +81,6 @@ int parseLedges(List *ledges, char *filename,
 	yxml_ret_t r;
 	yxml_t x[1];
 	yxml_init(x, stack, sizeof(stack));
-
-	
 
 	FILE *fp;
 	long lSize;
@@ -154,12 +94,10 @@ int parseLedges(List *ledges, char *filename,
 	
 	while((c = getc(fp)) != EOF) {
 		r = yxml_parse(x, c);
-		y_printres(x, r);
+		y_printres(x, r, ledges);
 	}
 
 	fclose(fp);
-	/* y_printtoken(x, yxml_eof(x) < 0 ? "error\n" : "ok\n"); */
-
 	return list_size(ledges);
 }
 
