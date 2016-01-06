f0ed920605f75bf4e0cf411b6a89f942695a1d1a
Sergi Pasoev <s.pasoev@gmail.com>
2016-01-06 22:53:39 +0400

    first steps to a better movevment

--- src/sprite.c
+++ src/sprite.c
@@ -39,3 +39,50 @@ void drawSprite(struct Sprite *sprite, struct Camera *camera,
 	DrawImageFrame(sprite->texture, sprite->pos.x, sprite->pos.y, sprite->w,
 		       sprite->h, 1, sprite->currentFrame, 0, flip, renderer);
 }
+
+public void update(float deltaTime) {
+        stateTime += deltaTime;
+        if (body == null) {
+		updateMotionX(deltaTime);
+		updateMotionY(deltaTime);
+
+		// Move to new position
+		position.x += velocity.x * deltaTime;
+		position.y += velocity.y * deltaTime;
+        } else {
+		position.set(body.getPosition());
+		rotation = body.getAngle() * MathUtils.radiansToDegrees;
+        }
+}
+
+void updateMotionX(float deltaTime) {
+        if (velocity.x != 0) {
+		// Apply friction
+		if (velocity.x > 0) {
+			velocity.x = Math.max(velocity.x - friction.x * deltaTime, 0);
+		} else {
+			velocity.x = Math.min(velocity.x + friction.x * deltaTime, 0);
+		}
+        }
+        // Apply acceleration
+        velocity.x += acceleration.x * deltaTime;
+        // Make sure the object's velocity does not exceed the
+        // positive or negative terminal velocity
+        velocity.x = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
+}
+
+void updateMotionY(float deltaTime) {
+        if (velocity.y != 0) {
+		// Apply friction
+		if (velocity.y > 0) {
+			velocity.y = Math.max(velocity.y - friction.y * deltaTime, 0);
+		} else {
+			velocity.y = Math.min(velocity.y + friction.y * deltaTime, 0);
+		}
+        }
+        // Apply acceleration
+        velocity.y += acceleration.y * deltaTime;
+        // Make sure the object's velocity does not exceed the
+        // positive or negative terminal velocity
+        velocity.y = MathUtils.clamp(velocity.y, -terminalVelocity.y, terminalVelocity.y);
+}
